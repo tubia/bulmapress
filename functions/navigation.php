3e44f0958b9340bb1dbbe4ef3103131af9ce0872
Egor Kirpichev <egor.kirpichev@gmail.com>
2019-03-02 18:12:16 +0100

    refactor: wrap function calls with if ! function_exists

--- functions/navigation.php
+++ functions/navigation.php
@@ -6,20 +6,24 @@
  */
 
 // This theme uses wp_nav_menu() in one location.
-register_nav_menus( array(
-	'menu-1' => esc_html__( 'Primary', 'bulmapress' ),
-	) );
+if ( ! function_exists( 'register_nav_menus' ) ) {
+	register_nav_menus( array(
+		'menu-1' => esc_html__( 'Primary', 'bulmapress' ),
+		) );
+}
 
 // Bulmapress navigation
-function bulmapress_navigation()
-{
-	wp_nav_menu( array(
-		'theme_location'    => 'menu-1',
-		'depth'             => 0,
-		'container'         => 'div id="navigation"',
-		'menu_class'        => 'navbar-end',
-		'fallback_cb'       => 'bulmapress_navwalker::fallback',
-		'walker'            => new bulmapress_navwalker()
-		)
-	);
+if ( ! function_exists( 'bulmapress_navigation' ) ) {
+	function bulmapress_navigation()
+	{
+		wp_nav_menu( array(
+			'theme_location'    => 'menu-1',
+			'depth'             => 0,
+			'container'         => 'div id="navigation"',
+			'menu_class'        => 'navbar-end',
+			'fallback_cb'       => 'bulmapress_navwalker::fallback',
+			'walker'            => new bulmapress_navwalker()
+			)
+		);
+	}
 }
